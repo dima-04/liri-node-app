40dedceccd1524bc793189fa905f9bc182c8d645
Dima <dima.dibb1@gmail.com>
2019-10-26 00:05:38 -0700

    call bands in town

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const axios = require("axios");
 const keys = require("./keys.js");
 
 const Spotify = require('node-spotify-api');
@@ -9,6 +10,32 @@ const command = process.argv[2];
 const arg = process.argv[3];
 
 function concertThis(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    // If the axios was successful...
+    // Then log the body from the site!
+    console.log(JSON.stringify(response.data));
+  },
+
+  function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  }
+);
+
 
 }
 function spotifyThisSong(song) {
