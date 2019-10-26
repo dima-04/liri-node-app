f50bf37f745f089e4e1bf3a1afec0c2d032877a3
Dima <dima.dibb1@gmail.com>
2019-10-25 23:51:15 -0700

    add the sign

--- liri.js
+++ liri.js
@@ -12,24 +12,27 @@ function concertThis(artist) {
 
 }
 function spotifyThisSong(song) {
-
     spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        if (data.tracks.items.length === 0) {
+            console.log("Artist");
 
-    const track = data.tracks.items[0];
+            console.log("Song Name: " + "The Sing");
+            console.log("Preview URL: " + "https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE");
+            return console.log("Album Name: " + "Happy Nation");
+        }
+        const track = data.tracks.items[0];
 
-    console.log("Artist");
-    for(let i=0;i<track.artists.length;i++){
-        console.log("-" + track.artists[i].name);
-    }
-    console.log("Song Name: " + track.name);
-    console.log("Preview URL: " + track.preview_url);
-    console.log("Album Name: " + track.album.name);
+        console.log("Artist");
+        for (let i = 0; i < track.artists.length; i++) {
+            console.log("-" + track.artists[i].name);
+        }
+        console.log("Song Name: " + track.name);
+        console.log("Preview URL: " + track.preview_url);
+        console.log("Album Name: " + track.album.name);
     });
-
-
 }
 function movieThis(movie) {
 
